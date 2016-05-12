d953d0307b165cf75525426c10995b32a99186cc
joelliew91 <joelliew91@gmail.com>
2016-05-12 10:50:50 +0800

    nested class

--- prog1.cs
+++ prog1.cs
@@ -5,6 +5,17 @@ namespace Test{
     class Prog{
         public static void Main(string [] args){
             Console.WriteLine("This is a Test");
+            Test.innertest.Prog2 newvar = new Test.innertest.Prog2();
+            newvar.sayhello();
+        }
+    }
+    
+    namespace innertest
+    {
+        public class Prog2{
+            public void sayhello(){
+                Console.WriteLine("This is from nested class"); 
+            }
         }
     }
 }
